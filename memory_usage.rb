0ed7c7987b1f6a7cccb1328c660a041bfb6c38ba
senseizp <senseizp@gmail.com>
2016-10-26 13:42:29 +0300

    version 2 add RU lang

--- memory_usage.rb
+++ memory_usage.rb
@@ -1,3 +1,11 @@
+if (Gem.win_platform?)
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
 
 def memory_usage
 
